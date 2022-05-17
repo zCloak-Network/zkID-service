584f52c8573db22c9f06b77d62b5beae99657ef8
weiguangchao <3409885610@qq.com>
2022-05-17 11:47:34 +0800

    remove static resource config

--- src/config/plugin.ts
+++ src/config/plugin.ts
@@ -1,7 +1,6 @@
 import { EggPlugin } from 'egg';
 export default {
   logrotator: false, // disable when use @midwayjs/logger
-  static: true,
   cors: {
     enable: true,
     package: 'egg-cors',
